22b909f37aa4fdafa07f2d81ec506194ad48df2d
Seb1776 <sebastianwho1776@gmail.com>
2021-05-14 02:08:27 +0000

    Added monty_functions, push implemented

---
+++ monty_functions.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * push - Push an element into the stack
+ *
+ * @stack: Given stack
+ * @line: Amount of lines
+ *
+ * Return: void
+ */
+void push(stack_t **stack, unsigned int line)
+{
+	char *c = global.argument;
+
+	if ((is_digit(c)) == 0)
+	{
+		fprintf(stderr, "L%d: usage push integer\n", line);
+		status = EXIT_FAILURE;
+		return;
+	}
+
+	if (global.data_struct == 1)
+	{
+		if (!add_node(stack, atoi(global.argument)))
+		{
+			return;
+			status = EXIT_FAILURE;
+		}
+	}
+
+	else
+	{
+		if (!queue_node(stack, atoi(global.argument)))
+		{
+			return;
+			status = EXIT_FAILURE;
+		}
+	}
+}
