e40b0f5a8ff470f3c28db28faf07442bf633be1e
Seb1776 <sebastianwho1776@gmail.com>
2021-05-14 03:08:58 +0000

    Implemented add

--- monty_functions_2.c
+++ monty_functions_2.c
@@ -27,3 +27,27 @@ void swap(stack_t **stack, unsigned int line)
 	tmp->n = tmp->next->n;
 	tmp->next->n = n;
 }
+
+/**
+ * addd - Sums the first two nodes of the stack
+ *
+ * @stack: Given stack
+ * @line: Ammount of lines
+ *
+ * Return: void
+ */
+void addd(stack_t **stack, unsigned int line)
+{
+	int res;
+
+	if (!stack || !*stack || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line);
+		status = EXIT_FAILURE;
+		return;
+	}
+
+	res = ((*stack)->next->n) + ((*stack)->n);
+	pop(stack, line);
+	(*stack)->n = res;
+}
